c919811a304224e06314afe6019d2de40bbfbd79
Michael <atlasbook@web.de>
2025-11-25 10:37:55 +0100

    04 / Refactoring Cat () 2

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,14 +5,13 @@ public class Cat {
   private String firstName;
   private String furColor;
   private int age; 
-  private boolean isFemale;
   private int counter;
 
-  public Cat(String firstName, String furColor, int age, boolean isFemale) {
+  
+  public Cat(String firstName, String furColor, int age) {
     this.firstName = firstName;
     this.furColor = furColor;
     this.age = age;
-    this.isFemale = isFemale;
   }
 
   public String getStringAttributes(String op) {
@@ -27,13 +26,9 @@ public class Cat {
   }
 
   public String getAge() {
-    if (!isFemale) {
-      return String.valueOf(age);
-    } else {
       return checkCompliance();
-    }
-    
   }
+    
 
   private String checkCompliance(){
     counter++;
@@ -46,6 +41,6 @@ public class Cat {
                 return "Talk to the hand!"; // 3rd time 
             default:
                 return "1#!?&&%"; //4th ++
-      }
+    }
   }
 }  
